00da39b881ac64c0d4f6ff70d9622e162bcc8baf
Xinyang <xinyzhang9@gmail.com>
2016-10-04 23:35:38 -0700

    class 3

--- bin/js/Game.js
+++ bin/js/Game.js
@@ -4,7 +4,12 @@ var Game = (function () {
         Laya.init(480, 852);
         var bg = new Background();
         Laya.stage.addChild(bg);
+        Laya.loader.load('res/atlas/war.json', Laya.Handler.create(this, this.onLoaded), null, Laya.Loader.ATLAS);
     }
+    Game.prototype.onLoaded = function () {
+        var hero = new Role();
+        Laya.stage.addChild(hero);
+    };
     return Game;
 }());
 new Game();
--- src/Game.ts
+++ src/Game.ts
@@ -5,6 +5,12 @@ class Game{
         Laya.init(480,852);
         var bg: Background = new Background();
         Laya.stage.addChild(bg);
+
+        Laya.loader.load('res/atlas/war.json',Laya.Handler.create(this,this.onLoaded),null,Laya.Loader.ATLAS);
+    }
+    onLoaded(){
+        var hero: Role = new Role();
+        Laya.stage.addChild(hero);
     }
     
 }
